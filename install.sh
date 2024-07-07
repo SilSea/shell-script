ad066d7c30045332e1bb670493e13560c53ee510
Nattawut <114056252+SilSea@users.noreply.github.com>
2024-07-07 23:28:21 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ GREEN="\e[1;32m"
 END="\e[0m"
 
 echo -e "${RED}Install Package SSH${END}"
-sudo apt install openssh-server -y
+&>/dev/null sudo apt install openssh-server -y
 echo -e "${GREEN}Package Install Success${END}"
 
 echo -e "${RED}Start Package SSH${END}"
-sudo systemctl enable ssh.service
-sud systemctl start ssh.service
+&>/dev/null sudo systemctl enable ssh.service
+&>/dev/null sudo systemctl start ssh.service
 echo -e "${GREEN}Package Start Success${END}"
 
 echo -e "${GREEN}All Success${END}"
